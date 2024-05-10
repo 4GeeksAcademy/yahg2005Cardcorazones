97076c1a4dd2d4c336112d60d7533263fc8cc6a8
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-05-10 19:09:35 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateCard() {
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Rey",
+    "Reina",
+    "Jota",
+    "As"
+  ];
+  let symbols = ["♥", "♠", "♣", "♦"];
+  let cardNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let cardSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  console.log(cardNumber);
+  let carta = document.getElementById("carta");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let numberElement = carta.querySelector(".number");
+  let topSymbol = carta.querySelector(".symbol.top-left");
+  let bottomSymbol = carta.querySelector(".symbol.bottom-right");
+
+  numberElement.innerText = cardNumber;
+  topSymbol.innerText = cardSymbol;
+  bottomSymbol.innerText = cardSymbol;
+  carta.innerHTML = cardNumber + " " + cardSymbol;
+  carta.className = "card " + cardSymbol.toLowerCase();
+  carta.classList.remove("heart", "spade", "club", "diamond");
+  if (cardSymbol === "♥") {
+    carta.classList.add("heart");
+  } else if (cardSymbol === "♠") {
+    carta.classList.add("spade");
+  } else if (cardSymbol === "♣") {
+    carta.classList.add("club");
+  } else if (cardSymbol === "♦") {
+    carta.classList.add("diamond");
+  }
+}
+
+window.onload = generateCard;
+
+let generarCartaButton = document.getElementById("generarCarta");
+generarCartaButton.addEventListener("click", generateCard);
